c96d29c21d6aac2e864e23cf9eb6fac8d5fdf599
KaloyanBehov <kaloqnbehov@gmail.com>
2023-12-07 10:23:40 +0300

    solution for isValid

---
+++ easy/valid_Parentheses.py
@@ -0,0 +1,31 @@
+'''Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.'''
+
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        brackets = {')': '(', ']': '[', '}': '{'}
+
+        for char in s:
+            if char in brackets.values():
+                stack.append(char)
+            elif char in brackets.keys():
+                if not stack or stack.pop() != brackets[char]:
+                    return False
+
+        return not stack
+
+
+solution = Solution()
+
+solution.isValid('(){}[]')
