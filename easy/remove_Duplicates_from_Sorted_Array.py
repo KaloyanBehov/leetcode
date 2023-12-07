a1e05e7f0591ab38e6f865174349618a26fdb0ba
KaloyanBehov <kaloqnbehov@gmail.com>
2023-12-07 09:47:00 +0300

    solution for removeDuplicates

---
+++ easy/remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,34 @@
+'''
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+'''
+
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0  # If the list is empty, there are no unique elements
+
+        k = 1  # Pointer for the position of the next unique element
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
+
+
+test = Solution()
+
+print(test.removeDuplicates([1,1,2]))
+
+
